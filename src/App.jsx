b3eb94a11e8d3a48affc4d2fae5f82d897415905
rebo <reggie@scerbo.dev>
2023-07-19 01:00:39 -0400

    add multi page support with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HowTo from './components/HowTo';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HowTo from './pages/HowTo';
+import Test from './pages/Test';
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return(
-    <HowTo />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="/" element={<HowTo />} />
+          <Route path="/react-esbuild-guide" element={<Test />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CodeBlock, github } from 'react-code-blocks';
+
+const Test = () => {
+    return(
+        <div>
+            <h1>Test Page</h1>
+        </div>
+    );
+};
+
+export default Test;
